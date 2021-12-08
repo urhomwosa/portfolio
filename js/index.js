5730c370042b399074e8e023630edb5ace153f47
urhomwosa <95337784+urhomwosa@users.noreply.github.com>
2021-12-08 22:25:48 +0300

    refactor: prettify js code

--- js/index.js
+++ js/index.js
@@ -1,20 +1,21 @@
 const navToggle = document.querySelector('.nav-toggle');
-const navLinks = document.querySelectorAll('.nav__link')
+const navLinks = document.querySelectorAll('.nav__link');
+const closeToggle = document.querySelector('.closebtn');
+
 
 navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-open');
 });
 
 
-const closeToggle = document.querySelector('.closebtn');
-
 closeToggle.addEventListener('click', () => {
     document.body.classList.remove("nav-open");
     document.body.classList.toggle('nav-close');
 });
 
+
 navLinks.forEach(link => {
     link.addEventListener('click', () => {
         document.body.classList.remove('nav-open');
-    })
-})
+    });
+});
